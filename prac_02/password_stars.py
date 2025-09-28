92e46ebdfef924de76ffef9a15e2a32286bfdd1b
lanlanqian <siqian.li@my.jcu.edu.au>
2025-09-28 12:20:19 +0800

    Add a parameter in get_valid_function and add some details on that docstring.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,7 +3,7 @@ MINIMUM_LENGTH = 4
 
 def main():
     """Run the password program"""
-    password = get_valid_password()
+    password = get_valid_password(MINIMUM_LENGTH)
     print_asterisks(password)
 
 
@@ -12,12 +12,12 @@ def print_asterisks(password, symbol='*'):
     print(len(password) * symbol)
 
 
-def get_valid_password():
-    """Validate the password"""
-    password = input(f"Enter password(at least {MINIMUM_LENGTH} characters): ")
-    while len(password) < MINIMUM_LENGTH:
+def get_valid_password(minimum_length):
+    """Validate the password based on the minimum length"""
+    password = input(f"Enter password(at least {minimum_length} characters): ")
+    while len(password) < minimum_length:
         print("Invalid password")
-        password = input(f"Enter password(at least {MINIMUM_LENGTH} characters): ")
+        password = input(f"Enter password(at least {minimum_length} characters): ")
     return password
 
 
