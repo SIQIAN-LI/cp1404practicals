ea545c63603337c0bbfacd88b4a784dc3fe79826
lanlanqian <siqian.li@my.jcu.edu.au>
2025-09-28 13:00:04 +0800

    Add two parameters in get_valid_score

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -14,12 +14,12 @@ MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
 
 def main():
     """Display a menu to get score, print result, show stars, or quit."""
-    score = get_valid_score()
+    score = get_valid_score(LOWEST_SCORE, HIGHEST_SCORE)
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            score = get_valid_score()
+            score = get_valid_score(LOWEST_SCORE, HIGHEST_SCORE)
         elif choice == "P":
             print_result(score)
         elif choice == "S":
@@ -31,10 +31,10 @@ def main():
     print("Farewell")
 
 
-def get_valid_score():
+def get_valid_score(lowest_score, highest_score):
     """Prompt for a valid score (0–100) and return it."""
     score = int(input("Score: "))
-    while score > HIGHEST_SCORE or score < LOWEST_SCORE:
+    while score > highest_score or score < lowest_score:
         print("Invalid score")
         score = int(input("Score: "))
     return score
@@ -55,7 +55,7 @@ def determine_result(score):
         return "Bad"
 
 
-def show_stars(score, symbol = '*'):
+def show_stars(score, symbol='*'):
     """Display the stars based on the given score."""
     print(symbol * score)
 
