1ae638b0b56ec981d4893c4d8c107fb94d16494b
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-03 20:11:07 +0800

    Finish question1 and 2 using open and lose to save the file.

---
+++ prac_03/files.py
@@ -0,0 +1,13 @@
+# 1.
+out_file = open("name.txt", "w")
+name = input("Enter your name: ")
+print(name, file=out_file)
+out_file.close()
+
+# 2.
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+print(f"Hi, {name}!")
+in_file.close()
+
+# 3.
