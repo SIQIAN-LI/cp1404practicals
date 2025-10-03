b764f31e94a755ef74b4828923b12b0e2296103f
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-03 21:10:47 +0800

    Make two filenames as contant.

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,25 +1,27 @@
+NAMAE_FILENAME = "name.txt"
+NUMBER_FILENAME = "numbers.txt"
+
 # 1.
-out_file = open("name.txt", "w")
+out_file = open(NAMAE_FILENAME, "w")
 name = input("Enter your name: ")
 print(name, file=out_file)
 out_file.close()
 
 # 2.
-in_file = open("name.txt", "r")
+in_file = open(NAMAE_FILENAME, "r")
 name = in_file.read().strip()
 print(f"Hi, {name}!")
 in_file.close()
 
 # 3.
-with open("numbers.txt", "r") as in_file:
+with open(NUMBER_FILENAME, "r") as in_file:
     line_1 = int(in_file.readline().strip())
     line_2 = int(in_file.readline().strip())
 print(line_1 + line_2)
 
-
-#4
+# 4
 total = 0
-with open("numbers.txt", "r") as in_file:
+with open(NUMBER_FILENAME, "r") as in_file:
     for line in in_file:
         number = int(line.strip())
         total += number
