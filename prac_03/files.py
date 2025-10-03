15af3263dc67b60e8792f91899a08b696557102c
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-03 21:14:22 +0800

    Delete repeated strip function

--- prac_03/files.py
+++ prac_03/files.py
@@ -15,14 +15,14 @@ in_file.close()
 
 # 3.
 with open(NUMBER_FILENAME, "r") as in_file:
-    line_1 = int(in_file.readline().strip())
-    line_2 = int(in_file.readline().strip())
+    line_1 = int(in_file.readline())
+    line_2 = int(in_file.readline())
 print(line_1 + line_2)
 
 # 4
 total_number = 0
 with open(NUMBER_FILENAME, "r") as in_file:
     for line in in_file:
-        number = int(line.strip())
+        number = int(line)
         total_number += number
 print(total_number)
