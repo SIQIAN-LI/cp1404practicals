4016c61b7a660c1145e52fbda4a189e049ac8789
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-06 12:53:35 +0800

    Complete load_data function and add an empty list to store each line of file

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -11,8 +11,10 @@ def main():
     print(data)
 
 
+#
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(filename)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +24,10 @@ def load_data(filename=FILENAME):
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data.append(parts)
         print("----------")
     input_file.close()
+    return data
 
 
 main()
