c9b22b24f3bcdefc8d3ab900f8da712635e0b8ac
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-10 14:23:36 +0800

    Generate sorted number lists

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,16 @@
+import random
+
+MAXIMUM_NUMBER = 25
+MINIMUM_NUMBER = 1
+NUMBER_OF_NUMBERS = 6
+
+number_of_picks = int(input("How many quick picks? "))
+for i in range(number_of_picks):
+    numbers = []
+    while len(numbers) < 6:
+        number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        if number not in numbers:
+            numbers.append(number)
+    numbers.sort()
+
+    print(numbers)
