1df897eb7e1ca6d002361d6ba973a20defa53ade
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-10 20:27:50 +0800

    Factorize the part of generating numbers per line into a function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,16 +1,27 @@
 import random
 
-MAXIMUM_NUMBER = 25
+MAXIMUM_NUMBER = 45
 MINIMUM_NUMBER = 1
 NUMBERS_PER_LINE = 6
 
-number_of_picks = int(input("How many quick picks? "))
-for i in range(number_of_picks):
-    numbers = []
-    while len(numbers) < NUMBERS_PER_LINE:
+
+def main():
+    """Get the number of picks and display these randomly selected numbers in ascending order."""
+    number_of_picks = int(input("How many quick picks? "))
+    for i in range(number_of_picks):
+        numbers = generate_quick_pick()
+        numbers.sort()
+        print(' '.join([f"{number:>2}" for number in numbers]))
+
+
+def generate_quick_pick():
+    """Generate six random and no-repeating numbers in a list."""
+    random_numbers = []
+    while len(random_numbers) < NUMBERS_PER_LINE:
         number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
-        if number not in numbers:
-            numbers.append(number)
-    numbers.sort()
-    result = ' '.join([f"{number:>2}" for number in numbers])
-    print(result)
+        if number not in random_numbers:
+            random_numbers.append(number)
+    return random_numbers
+
+
+main()
