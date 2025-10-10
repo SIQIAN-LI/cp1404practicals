e98127491eef20504818b323e24bdecf51cacc41
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-10 12:15:03 +0800

    Finish security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,5 +1,3 @@
-from prac_01.broken_score import message
-
 numbers = []
 for i in range(5):
     number = int(input("Number: "))
@@ -14,4 +12,12 @@ print(f"The smallest number is {min_number}")
 print(f"The largest number is {max_number}")
 print(f"The average of the numbers is {average}")
 
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
+username = input("Enter your username: ")
+if username in usernames:
+    message = "Access granted"
+else:
+    message = "Access denied"
+print(message)
