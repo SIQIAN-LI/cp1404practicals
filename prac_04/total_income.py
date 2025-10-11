a47f31e01a25bb5791596720385a7ca7bb6ddbe3
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-11 21:24:10 +0800

    Use enumerate funcion to make display_report function better

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,17 +13,16 @@ def main():
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
-    display_report(incomes, number_of_months)
+    display_report(incomes)
 
 
-def display_report(incomes, number_of_months):
+def display_report(incomes):
     """Display an income report based on a list of incomes and number of months."""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes, 1):
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
 main()
