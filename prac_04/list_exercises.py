6673afbf91f56fcd2df1d2edd2e60895969ddafb
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-11 20:57:38 +0800

    Adjust formats and fix index for showing last number

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,5 +1,8 @@
+# Basic list operations
+NUMBERS_COUNT = 5
+
 numbers = []
-for i in range(5):
+for i in range(NUMBERS_COUNT):
     number = int(input("Number: "))
     numbers.append(number)
 max_number = max(numbers)
@@ -7,13 +10,16 @@ min_number = min(numbers)
 average = sum(numbers) / len(numbers)
 
 print(f"The first number is {numbers[0]}")
-print(f"The last number is {numbers[4]}")
+print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min_number}")
 print(f"The largest number is {max_number}")
 print(f"The average of the numbers is {average}")
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
-             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+# Woefully inadequate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+             'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command',
+             'ExecState', 'InteractiveConsole', 'InterpreterInterface',
+             'StartServer', 'bob']
 
 username = input("Enter your username: ")
 if username in usernames:
