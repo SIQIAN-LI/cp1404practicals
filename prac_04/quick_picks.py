09b194f707ed6a64ef59897cbaf27c2935b7b16d
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-11 21:04:47 +0800

    Change function name

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,12 +9,12 @@ def main():
     """Get the number of picks and display these randomly selected numbers in ascending order."""
     number_of_picks = int(input("How many quick picks? "))
     for i in range(number_of_picks):
-        numbers = generate_quick_pick()
+        numbers = generate_quick_pick_per_line()
         numbers.sort()
         print(' '.join([f"{number:>2}" for number in numbers]))
 
 
-def generate_quick_pick():
+def generate_quick_pick_per_line():
     """Generate six random and no-repeating numbers in a list."""
     random_numbers = []
     while len(random_numbers) < NUMBERS_PER_LINE:
