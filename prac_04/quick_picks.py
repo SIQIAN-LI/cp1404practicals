ffed29b52f51ffd1c482baf6c400549c2334f1bd
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-11 21:32:44 +0800

    Change variable name numbers to random_numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,9 +9,9 @@ def main():
     """Get the number of picks and display these randomly selected numbers in ascending order."""
     number_of_picks = int(input("How many quick picks? "))
     for i in range(number_of_picks):
-        numbers = generate_quick_pick_per_line()
-        numbers.sort()
-        print(' '.join([f"{number:>2}" for number in numbers]))
+        random_numbers = generate_quick_pick_per_line()
+        random_numbers.sort()
+        print(' '.join([f"{number:>2}" for number in random_numbers]))
 
 
 def generate_quick_pick_per_line():
