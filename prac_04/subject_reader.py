cf3c9e9cf1447d7c8242dfdb902546dcdd199748
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-12 21:26:46 +0800

    Change 2 into student count index

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -20,13 +20,13 @@ def load_subject_records(filename=FILENAME):
     subject_records = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+        # print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[STUDENT_COUNT_INDEX] = int(parts[STUDENT_COUNT_INDEX])  # Make the number an integer (ignore PyCharm's warning)
+        # print(parts)  # See if that worked
         subject_records.append(parts)
         print("----------")
     input_file.close()
