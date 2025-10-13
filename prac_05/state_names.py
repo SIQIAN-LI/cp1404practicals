b8e28828331079c8031d75d1ae891f2020e1983c
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-13 23:29:31 +0800

    Use for loop to print all state codes and names neatly (state code left-aligned)

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,17 +6,18 @@ File needs reformatting
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {
-                "QLD": "Queensland",
-                "NSW": "New South Wales",
-                "NT": "Northern Territory",
-                "WA": "Western Australia",
-                "ACT": "Australian Capital Territory",
-                "VIC": "Victoria",
-                "TAS": "Tasmania", "SA": "South Australia"
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania", "SA": "South Australia"
 }
 
 print(CODE_TO_NAME)
 max_state_code_width = max(len(state_code) for state_code in CODE_TO_NAME.keys())
+
 state_code = input("Enter short state: ").strip().upper()
 while state_code != "":
     try:
@@ -24,3 +25,6 @@ while state_code != "":
     except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").strip().upper()
+
+for state_code, state_name in CODE_TO_NAME.items():
+    print(f"{state_code:{max_state_code_width}} is {CODE_TO_NAME[state_code]}")
