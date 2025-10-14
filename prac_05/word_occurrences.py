62f786465805298044480f472639cb5aab72dd2a
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-14 11:20:53 +0800

    Complete sorted and formated display part

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -9,5 +9,8 @@ word_to_count = {}
 for word in words:
     frequency = word_to_count.get(word, 0)
     word_to_count[word] = frequency + 1
-print(word_to_count)
 
+max_word_width = max(len(word) for word in word_to_count.keys())
+
+for word, count in sorted(word_to_count.items()):
+    print(f"{word:{max_word_width}} : {count}")
