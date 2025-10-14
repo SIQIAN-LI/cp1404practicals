c4d95190310e2e2e7a51f8fb12f54e86f2c7e3fc
lanlanqian <siqian.li@my.jcu.edu.au>
2025-10-14 18:40:51 +0800

    Complete process records part to count champions and countries

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,11 +4,15 @@ Estimate: 25 minutes
 Actual:   20 minutes
 """
 FILENAME = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
 
 
 def main():
     records = load_records(FILENAME)
-    print(records)
+    champion_to_count, countries = process_records(records)
+    print(champion_to_count)
+    print(countries)
 
 
 def load_records(filename):
@@ -16,6 +20,15 @@ def load_records(filename):
         in_file.readline()
         records = [record.strip().split(',') for record in in_file.readlines()]
     return records
+
+
+def process_records(records):
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[COUNTRY_INDEX])
+        champion_to_count[record[CHAMPION_INDEX]] = champion_to_count.get(record[CHAMPION_INDEX], 0) + 1
+    return champion_to_count, countries
 
 
 main()
